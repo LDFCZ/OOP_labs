0aa1d38ffddda8a2f68e3ce70407e61af8c0977b
Василий Лопаткин <v.lopatkin1@g.nsu.ru>
2022-03-10 14:10:26 +0700

    fix runtimeEx

--- Java/lab2/src/Java/exceptions/CommandRunTimeException.java
+++ Java/lab2/src/Java/exceptions/CommandRunTimeException.java
@@ -2,7 +2,7 @@ package Java.exceptions;
 
 import Java.constspace.CONSTSPACE;
 
-public class CommandRunTimeException extends Exception {
+public class CommandRunTimeException extends RuntimeException {
     public CommandRunTimeException (String commandName, String message) {
         super(commandName + CONSTSPACE.SPACE + message);
     }
