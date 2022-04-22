ed5331bd59ab17f264994403c1499431536de64a
Василий Лопаткин <v.lopatkin1@g.nsu.ru>
2022-04-22 13:12:35 +0700

    fix commands exceptions

--- Java/lab2/src/main/java/lopatkin/lab2/exceptions/CommandRunTimeException.java
+++ Java/lab2/src/main/java/lopatkin/lab2/exceptions/CommandRunTimeException.java
@@ -3,7 +3,8 @@ package lopatkin.lab2.exceptions;
 
 import lopatkin.lab2.constspace.ConstSpace;
 
-public class CommandRunTimeException extends RuntimeException {
+//This is Exception! Not RunTime! It is just a name. There are very much differences between runtime and simple Exceptions!
+public class CommandRunTimeException extends Exception {
     public CommandRunTimeException (String commandName, String message) {
         super(commandName + ConstSpace.SPACE + message);
     }
