7ab680ea8f1ed45b0c9908605d121fa7793751a7
Василий Лопаткин <v.lopatkin1@g.nsu.ru>
2022-05-10 17:57:28 +0700

    WIP start to write server side and bubble messages :)

--- Java/lab5/clientSpringBoot/src/main/java/ru/nsu/ccfit/lopatkin/client/controllers/ChatController.java
+++ Java/lab5/clientSpringBoot/src/main/java/ru/nsu/ccfit/lopatkin/client/controllers/ChatController.java
@@ -1,13 +1,16 @@
 package ru.nsu.ccfit.lopatkin.client.controllers;
 
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
+import ru.nsu.ccfit.lopatkin.client.utils.BubbleMessages;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,7 +26,7 @@ public class ChatController {
     private URL location;
 
     @FXML
-    private ListView<?> chatPane;
+    private ListView chatPane;
 
     @FXML
     private Button getUserListButton;
@@ -54,6 +57,36 @@ public class ChatController {
         assert messageField != null : "fx:id=\"messageField\" was not injected: check your FXML file 'chat_page.fxml'.";
         assert nameLabel != null : "fx:id=\"nameLabel\" was not injected: check your FXML file 'chat_page.fxml'.";
         assert sendButton != null : "fx:id=\"sendButton\" was not injected: check your FXML file 'chat_page.fxml'.";
+
+        Task<VBox> message = new Task<VBox>() {
+            @Override
+            protected VBox call() throws Exception {
+                Label name= new Label("TestName");
+                BubbleMessages text = new BubbleMessages("test\ntext 123321123321 вававава", "11:12");
+                VBox v = new VBox();
+                v.getChildren().addAll(name, text);
+                return v;
+            }
+        };
+        Task<VBox> message2 = new Task<VBox>() {
+            @Override
+            protected VBox call() throws Exception {
+                Label name= new Label("TestName");
+                BubbleMessages text = new BubbleMessages("test2\ntext 123321123321 вававава", "11:12");
+                VBox v = new VBox();
+                v.getChildren().addAll(name, text);
+                return v;
+            }
+        };
+
+        message.setOnSucceeded(event -> chatPane.getItems().add(message.getValue()));
+        Thread t2 = new Thread(message);
+        t2.setDaemon(true);
+        t2.start();
+        message2.setOnSucceeded(event -> chatPane.getItems().add(message2.getValue()));
+        Thread t1 = new Thread(message2);
+        t1.setDaemon(true);
+        t1.start();
 
     }
 
